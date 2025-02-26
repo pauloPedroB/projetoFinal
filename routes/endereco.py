4c1f08c43588e9fa90ba9422d009e13354283edd
lucas <ahissareis@gmail.com>
2025-02-26 20:15:32 -0300

    validacao

--- routes/endereco.py
+++ routes/endereco.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 from classes import db,Endereco
+import requests
+import re
 
 endereco_bp = Blueprint('endereco', __name__)
 
@@ -42,3 +44,57 @@ def cadastrar():
 def validarCadastroEndereco(cep,rua, numero, bairro, complemento, uf):
     print('/Tem que fazer')
 
+
+
+
+
+def validar_formulario(cep, numero, complemento, rua, cidade, uf):
+    MAX_RUA_LENGTH = 65
+    MAX_CIDADE_LENGTH = 40
+    MAX_CEP_LENGTH = 8
+    MAX_COMPLEMENTO_LENGTH = 200
+    MAX_UF_LENGTH = 2
+    
+    # Validação do CEP
+    if not re.fullmatch(r"\d{8}", cep):
+        return False,"CEP inválido. Deve conter exatamente 8 dígitos numéricos."
+    else:
+        response = requests.get(f"https://viacep.com.br/ws/{cep}/json/")
+        if response.status_code == 200:
+            cep_data = response.json()
+            if "erro" in cep_data:
+                return False,"CEP NÃO ENCONTRADO"
+            else:
+                rua = cep_data.get("logradouro", "")[:MAX_RUA_LENGTH]
+                cidade = cep_data.get("localidade", "")[:MAX_CIDADE_LENGTH]
+                uf = cep_data.get("uf", "")[:MAX_UF_LENGTH]
+        else:
+            return False, "Erro ao consultar o CEP."
+    
+    # Validação do número
+    if not numero:
+        return False, "Número é obrigatório."
+    
+    # Validação da rua
+    if not rua:
+        return False, "Rua é obrigatória."
+    elif len(rua) > MAX_RUA_LENGTH:
+        return False, f"Rua não pode ter mais que {MAX_RUA_LENGTH} caracteres."
+    
+    # Validação da cidade
+    if not cidade:
+        return False, "Cidade é obrigatória."
+    elif len(cidade) > MAX_CIDADE_LENGTH:
+        return False, f"Cidade não pode ter mais que {MAX_CIDADE_LENGTH} caracteres."
+    
+    # Validação da UF
+    if not uf:
+        return False, "UF é obrigatória."
+    elif len(uf) != MAX_UF_LENGTH:
+        return False, f"UF deve ter exatamente {MAX_UF_LENGTH} caracteres."
+    
+    # Validação do complemento
+    if len(complemento) > MAX_COMPLEMENTO_LENGTH:
+        return False,f"Complemento não pode ter mais que {MAX_COMPLEMENTO_LENGTH} caracteres."
+    
+    return True, [re.sub(r"\D", "", cep),rua,numero,complemento,uf,cidade]
