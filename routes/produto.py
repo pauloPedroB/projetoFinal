c58b9c2ff1f352d10cd0fc27f817f54ef03c3514
lucas <ahissareis@gmail.com>
2025-02-27 19:00:36 -0300

    excluir

--- routes/produto.py
+++ routes/produto.py
@@ -58,26 +58,44 @@ def cadastrar():
 
         if not file:
             return redirect(url_for("produto.cadastro", erro = "Imagem é obrigatória"))
-
-
         if not allowed_file(file.filename):
             return redirect(url_for("produto.cadastro", erro = "Formato de imagem inválido. Use PNG, JPG ou JPEG."))
-        
         filename = file.filename
         # Salva o arquivo na pasta de uploads
         file.save(os.path.join(current_app.config["UPLOAD_FOLDER"], filename))
         # Armazena o nome da imagem em um arquivo de texto
         with open("imagens.txt", "a") as f:
             f.write(filename + "\n")
-
         novo_produto = Produto(nome_produto = nome, img = filename)
         db.session.add(novo_produto)
         db.session.commit()
-
         return redirect(url_for('produto.produtos'))
     except:
         return redirect(url_for('produto.cadastro',erro = "Algo deu errado, tente novamente"))
-    
+
+@produto_bp.route('/excluir/<id>',methods=['POST'])
+def excluir(id):
+    try:
+        cadastro = validacoes.verificarCadastroCompleto()
+        if cadastro:
+            return cadastro
+        if session['typeUser'] != 1:
+            return render_template('menu/menu.html', mensagem="Você não possuí acesso de administrador",typeUser = session['typeUser'])
+            
+        mensagem = request.args.get('mensagem', "")
+
+        produto = Produto.query.get(id)
+        if not produto:
+            return redirect(url_for('produto.produtos',erro = "Produto não encontrado"))
+
+        db.session.delete(produto)
+        db.session.commit()
+      
+
+      
+        return redirect(url_for('produto.produtos', erro = "Produto deletado com sucesso"))
+    except:
+        return redirect(url_for('produto.cadastro',erro = "Algo deu errado, tente novamente"))
 
 def allowed_file(filename):
     """Verifica se o arquivo tem uma extensão permitida"""
