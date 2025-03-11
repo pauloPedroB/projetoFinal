9489bd46e41d5844abbee9f104c218642956c4e1
pauloPedroB <siqueirapedropaulo93@gmail.com>
2025-03-11 12:02:25 -0300

    editar endereco

--- routes/endereco.py
+++ routes/endereco.py
@@ -66,12 +66,75 @@ def cadastrar():
     except Exception as e:
         return redirect(url_for('endereco.cadastro', mensagem = f"Algo deu errado, tente novamente: {e}"))
 
+@endereco_bp.route('/editar/<id_endereco>')
+def editar(id_endereco):
+    try:
+        verificar = validacoes.verificarCadastro()
+        if verificar:
+            return verificar
+        verificarLojaCliente = validacoes.verificarLojaCliente()
+        if verificarLojaCliente:
+            return verificarLojaCliente
+        if session['typeUser'] == 1:
+            return redirect(url_for('menu.principal',mensagem = "Você não possuí acesso à essa página"))
+        mensagem = request.args.get('mensagem')
+        if mensagem == None:
+            mensagem = ""
 
+        endereco = Endereco.query.get(id_endereco)
+        if not endereco:
+            return redirect(url_for('menu.Principal',mensagem = "Endereço não encontrado"))
+        
+        return render_template('cadastroEnd.html',mensagem = mensagem, endereco = endereco)
+    except Exception as e:
+            return redirect(url_for('menu.principal',mensagem = f"Algo deu errado, tente novamente: {e}"))
        
+@endereco_bp.route('/update/<id_endereco>',methods=['POST'])
+def update(id_endereco):
+    try:
+        verificar = validacoes.verificarCadastro()
+        if verificar:
+            return verificar
+        verificarLojaCliente = validacoes.verificarLojaCliente()
+        if verificarLojaCliente:
+            return verificarLojaCliente
+        if session['typeUser'] == 1:
+            return redirect(url_for('menu.principal',mensagem = "Você não possuí acesso à essa página"))
+        mensagem = request.args.get('mensagem')
+        if mensagem == None:
+            mensagem = ""
 
+        endereco = Endereco.query.get(id_endereco)
+        if not endereco:
+            return redirect(url_for('menu.Principal',mensagem = "Endereço não encontrado"))
+        
+        cep = request.form['CEP']
+        rua = request.form['rua']
+        numero = request.form['numero']
+        bairro = request.form['bairro']
+        uf = request.form['uf']
+        cidade = request.form['cidade']
+        complemento = request.form['complemento']
+
+        validacao,mensagem = validar_endereco(cep, numero, complemento, rua, cidade, uf,bairro)
+        if validacao == False:
+            return redirect(url_for('endereco.editar', mensagem = mensagem))
+        endereco.cep=mensagem[0]
+        endereco.nmr = mensagem[1]
+        endereco.complemento = mensagem[2]
+        endereco.rua = mensagem[3]
+        endereco.cidade = mensagem[4]
+        endereco.uf = mensagem[5]
+        endereco.bairro = mensagem[6]
+        endereco.latitude = mensagem[7]
+        endereco.longitude = mensagem[8]
+
+        db.session.commit()
+        return redirect(url_for('menu.dados',mensagem = "Endereco editado"))
+
+    except Exception as e:
+            return redirect(url_for('menu.principal',mensagem = f"Algo deu errado, tente novamente: {e}"))
     
-
-
 
 def validar_endereco(cep, numero, complemento, rua, cidade, uf,bairro):
     MAX_RUA_LENGTH = 65
@@ -89,12 +152,21 @@ def validar_endereco(cep, numero, complemento, rua, cidade, uf,bairro):
             if "erro" in cep_data:
                 return False,"CEP NÃO ENCONTRADO"
             else:
-                rua = cep_data.get("logradouro", "")[:MAX_RUA_LENGTH]
-                cidade = cep_data.get("localidade", "")[:MAX_CIDADE_LENGTH]
-                uf = cep_data.get("uf", "")[:MAX_UF_LENGTH]
-                bairro = cep_data.get("bairro", "")
+                nova_rua = cep_data.get("logradouro", "")[:MAX_RUA_LENGTH]
+                nova_cidade = cep_data.get("localidade", "")[:MAX_CIDADE_LENGTH]
+                novo_uf = cep_data.get("uf", "")[:MAX_UF_LENGTH]
+                novo_bairro = cep_data.get("bairro", "")
         else:
             return False, "Erro ao consultar o CEP."
+        
+    if nova_rua != "":
+        rua = nova_rua
+    if nova_cidade != "":
+        cidade = nova_cidade
+    if novo_uf != "":
+        uf = novo_uf
+    if novo_bairro != "":
+        bairro = novo_bairro
     
     # Validação do número
     if not numero:
